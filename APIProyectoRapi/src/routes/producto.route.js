af1f4f90baf6f9ae5fffd09156eceb1a8a4cfd26
Javier Valencia <javierandres222@gmail.com>
2021-12-11 09:39:56 -0500

    Cargando final

--- APIProyectoRapi/src/routes/producto.route.js
+++ APIProyectoRapi/src/routes/producto.route.js
@@ -4,10 +4,10 @@ const auth = require('../Auth/Auth');
 
 const {getProductos, getProducto, createProducto, editProducto, deleteProducto } = require ('../controller/producto.controller');
 
-router.get('/productos', getProductos); // pedir todos los productos
-router.get('/producto/:codigo', getProducto); // pedir un solo producto
+router.post('/productos',auth, getProductos); // pedir todos los productos
+router.post('/producto/:codigo', auth, getProducto); // pedir un solo producto
 router.post('/producto', auth, createProducto); // Crear un producto
-router.put('/producto/:codigo', auth, editProducto); // Editar un producto
-router.delete('/producto/:codigo', auth, deleteProducto); // Eliminar un producto
+router.put('/producto/:codigo', editProducto); // Editar un producto
+router.delete('/producto/:codigo', deleteProducto); // Eliminar un producto
 
 module.exports = router;
